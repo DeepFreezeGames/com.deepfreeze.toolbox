e679279093d877681688a0ef717fe03552d012d5
Liam Knightley <liam.knightley@kanoapps.com>
2019-05-24 12:47:27 -0700

    Added replace selection tool

---
+++ Editor/ReplaceSelection.cs
@@ -0,0 +1,72 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Toolbox.Editor
+{
+    public class ReplaceSelection : ScriptableWizard
+    {
+        private static GameObject _replacement = null;
+        private static bool _keep = false;
+
+        public GameObject replacementObject = null;
+        public bool keepOriginals = false;
+
+        [MenuItem("Tools/Replace Selection")]
+        private static void CreateWizard()
+        {
+            DisplayWizard("Replace Selection", typeof(ReplaceSelection), "Replace");
+        }
+
+        public ReplaceSelection()
+        {
+            replacementObject = _replacement;
+            keepOriginals = _keep;
+        }
+
+        private void OnWizardUpdate()
+        {
+            _replacement = replacementObject;
+            _keep = keepOriginals;
+        }
+
+        private void OnWizardCreate()
+        {
+            if (_replacement == null)
+                return;
+
+            Undo.RegisterSceneUndo("Replace Selection");
+
+            var transforms = Selection.GetTransforms(SelectionMode.TopLevel | SelectionMode.OnlyUserModifiable);
+
+            foreach (var transform in transforms)
+            {
+                GameObject spawnedObject;
+                var prefab = PrefabUtility.GetPrefabAssetType(_replacement);
+
+                if (prefab == PrefabAssetType.Regular || prefab == PrefabAssetType.Model || prefab == PrefabAssetType.Variant)
+                {
+                    spawnedObject = (GameObject)PrefabUtility.InstantiatePrefab(_replacement);
+                }
+                else
+                {
+                    spawnedObject = GameObject.Instantiate(_replacement);
+                }
+
+                var spawnedObjectTransform = spawnedObject.transform;
+                spawnedObjectTransform.parent = transform.parent;
+                spawnedObject.name = _replacement.name;
+                spawnedObjectTransform.localPosition = transform.localPosition;
+                spawnedObjectTransform.localScale = transform.localScale;
+                spawnedObjectTransform.localRotation = transform.localRotation;
+            }
+
+            if (!_keep)
+            {
+                foreach (var gameObject in Selection.gameObjects)
+                {
+                    GameObject.DestroyImmediate(gameObject);
+                }
+            }
+        }
+    }
+}
